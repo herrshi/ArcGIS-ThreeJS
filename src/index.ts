9e9699895ad8ab3d9d0020ac300ed9fbd6e39312
晓青 史 <herrshi@hotmail.com>
2019-06-03 17:38:29 +0800

    11

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import "./config";
 
 import App from "./widgets/App";
 
-export const app = new App({
-  appName: "三维地图",
-  container: document.getElementById("app") as HTMLElement
-});
+class TGISMap {
+  public static async createMap() {
+    const app = new App({
+      appName: "三维地图",
+      container: document.getElementById("app") as HTMLElement
+    });
+  }
+}
+
+(window as any).TGISMap = TGISMap;
 
