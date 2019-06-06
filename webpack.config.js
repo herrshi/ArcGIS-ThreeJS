5c15f20c0dc4584d040698825752255c30ec2d01
晓青 史 <herrshi@hotmail.com>
2019-06-06 17:35:15 +0800

    11

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,21 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const path = require("path");
 
 module.exports = {
   entry: {
-    index: ["./src/css/main.scss", "@dojo/framework/shim/Promise", "./src/index.ts"]
+    index: [
+      "./src/css/main.scss",
+      "@dojo/framework/shim/Promise",
+      "./src/index.ts"
+    ]
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname,"web-gis"),
+    path: path.resolve(__dirname, "web-gis"),
     publicPath: "/web-gis/"
   },
   optimization: {
@@ -56,9 +60,7 @@ module.exports = {
           "sass-loader?sourceMap"
         ]
       },
-      {
-
-      }
+      {}
     ]
   },
   plugins: [
